db12f0527286b616bae7f25deeeab3cbac453bc8
Tom Schmidt <wocif@protonmail.com>
2025-03-29 00:43:26 +0100

    ground level 3d to 1.6 from 1.5

--- app.js
+++ app.js
@@ -143,7 +143,7 @@ const createScene = async function () {
     rootPilar.rotationQuaternion = new BABYLON.Quaternion();
 
     // lower ground of 3d scene
-    rootScene.position.y -= 1.5;
+    rootScene.position.y -= 1.6;
 
 
 
